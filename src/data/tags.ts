0754b8c6babf11275b4638083433ed035df85642
Ben Chau <benjaminchau05@gmail.com>
2024-11-28 08:07:23 +1000

    Delete src directory

--- src/data/tags.ts
+++
@@ -1,9 +0,0 @@
-export const yearTags = ['2021', '2022', '2023'] as const;
-export const typeTags = ['TA', 'TF'] as const;
-export const formatTags = ['MC', 'Written'] as const;
-export const filterTags = ['All', ...typeTags, ...yearTags, ...formatTags] as const;
-
-export type YearTag = typeof yearTags[number];
-export type TypeTag = typeof typeTags[number];
-export type FormatTag = typeof formatTags[number];
-export type FilterTag = typeof filterTags[number];
